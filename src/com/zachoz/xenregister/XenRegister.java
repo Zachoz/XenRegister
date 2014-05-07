7e8e7c94a549bb8b33f99e8d3c6e4b89e313f644
Zach de Koning <zach.de.koning@gmail.com>
2014-05-07 21:44:20 +1000

    All around improvements

--- src/com/zachoz/xenregister/XenRegister.java
+++ src/com/zachoz/xenregister/XenRegister.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.BufferedReader;
@@ -16,17 +17,25 @@ import java.util.Random;
 
 /**
  * Plugin to allow in-game registration for the XenForo forum software.
+ *
  * @author Zachoz
  */
 public class XenRegister extends JavaPlugin {
 
-    public static String site;
-    public static String apiHash;
+    private static String site, apiHash, usernameField, uuidField;
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     public void onEnable() {
         saveDefaultConfig();
         site = getConfig().getString("site");
         apiHash = getConfig().getString("apihash");
+        usernameField = getConfig().getString("custom_field_names.minecraft_username");
+        uuidField = getConfig().getString("custom_field_names.minecraft_uuid");
+
+        if (!site.substring(site.length() - 1).equals("/")) {
+            site = (site + "/");
+        }
     }
 
     public void onDisable() {
@@ -42,15 +51,9 @@ public class XenRegister extends JavaPlugin {
                 Bukkit.getScheduler().runTaskAsynchronously(this, new Runnable() {
                     public void run() {
                         Random ran = new Random();
-                        int x = ran.nextInt(Integer.MAX_VALUE) + 999999; // Generate random number password
-                        if (registerUser(sender.getName(), x + "", args[0])) {
-                            sender.sendMessage(ChatColor.DARK_AQUA + "Successfully registered on the forums!!");
-                            sender.sendMessage(ChatColor.DARK_AQUA + "A confirmation email should be " +
-                                    "sent to your email address shortly! Please change your password once you confirm your account!");
-                            sender.sendMessage(ChatColor.GOLD + site);
-                        } else {
-                            sender.sendMessage(ChatColor.RED + "Registration failed! An account using the same username or email already exists!");
-                        }
+                        long x = (long) ran.nextInt(Integer.MAX_VALUE) + 999999L; // Generate random number password
+                        String registrationStatus = registerUser(sender.getName(), x + "", args[0]);
+                        sender.sendMessage(registrationStatus);
                     }
                 });
             }
@@ -60,18 +63,30 @@ public class XenRegister extends JavaPlugin {
     }
 
     /**
-     * Registers a user on the xenregister
+     * Registers a user on the forums
      *
      * @param user     Username of the user
      * @param password Password for the user. A randomly generated one is recommended.
      * @param email    Email address for the user
-     * @return True if reqistration was successful
+     * @return Status of the registration attempt
      */
-    public static boolean registerUser(String user, String password, String email) {
+    private static String registerUser(String user, String password, String email) {
+        if (email.contains("=") || email.contains("&")) {
+            return ChatColor.RED + "Potential injection attack! This event has been recorded!";
+        }
+
+        if (!email.matches(EMAIL_PATTERN)) {
+            return ChatColor.RED + "Invalid email address!";
+        }
+
+        String uuid = "null";
+        Player p = Bukkit.getPlayer(user);
+        if (p != null) uuid = p.getUniqueId().toString();
+
         try {
-            String link = "api.php?action=register&hash=" + apiHash + "&username=" + user +
-                    "&password=" + password + "&email=" + email + "&custom_fields=minecraftusername=" + user
-                    + "&user_state=email_confirm";
+            String link = "api.php?action=register&hash=" + apiHash + "&username=" + user.replace('&', '.') +
+                    "&password=" + password + "&email=" + email.replace('&', '.').replace('=', '.') +
+                    "&custom_fields=" + usernameField + "=" + user + "," + uuidField + "=" + uuid + "&user_state=email_confirm";
 
             URL url = new URL(site + link);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // Open URL connection
@@ -83,23 +98,28 @@ public class XenRegister extends JavaPlugin {
                 if (inputLine.contains("{\"error\":7,\"message\":\"Something went wrong when \\\"registering user\\\": \\\"" +
                         "User already exists\\\"\",\"user_error_id\":40,\"user_error_field\":\"username\",\"" +
                         "user_error_key\":\"usernames_must_be_unique\",\"user_error_phrase\":\"Usernames must be unique." +
-                        " The specified username is already in use.\"}")) return false;
+                        " The specified username is already in use.\"}"))
+                    return ChatColor.RED + "A user under this username already exists!";
 
                 // Email already in use?
                 if (inputLine.contains("{\"error\":7,\"message\":\"Something went wrong when \\\"registering user\\\": \\\"" +
                         "Email already used\\\"\",\"user_error_id\":42,\"user_error_field\":\"email\",\"user_error_key\":\"" +
                         "email_addresses_must_be_unique\",\"user_error_phrase\":\"Email addresses must be unique. " +
-                        "The specified email address is already in use.\"}")) return false;
+                        "The specified email address is already in use.\"}"))
+                    return ChatColor.RED + "A user under this email address already exists!";
             }
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            return false;
+            return ChatColor.RED + "An internal error has occured! The stack trace has been printed to the server's console!";
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return ChatColor.RED + "An internal error has occured! The stack trace has been printed to the server's console!";
         }
-        return true;
+
+        return ChatColor.DARK_AQUA + "Successfully registered on the forums! \n" +
+                "A confirmation email should be sent to your email address shortly! Please change your" +
+                " password once you confirm your account!";
     }
 
 }
